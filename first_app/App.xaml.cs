1c87f48f55b443bdad07bfd449b4c37ba37f5628
jasaga2401 <jasaga@protonmail.com>
2023-12-21 12:51:35 +0000

    Add project files.

---
+++ first_app/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace first_app
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ first_app/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace first_app
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        string var_name;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            var_name = NameEntry.Text;
+
+            DisplayAlert("Alert", $"Hello {var_name}", "OK");
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+}
